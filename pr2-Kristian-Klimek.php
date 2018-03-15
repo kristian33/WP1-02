bc7df850228715db536c41fd05ff82b84e22214b
kristianklimek <klimekkristian@gmail.com>
2018-03-15 14:19:19 +0100

    Pr2 Koky, díky. JO, ČAU.

---
+++ pr2-Kristian-Klimek.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+
+<body>
+  <h1>Práce s proměnnou</h1>
+<?php
+$a = 1;
+$b = 2;
+$soucet = $a+$b;
+
+$soucin = $a*$b;
+
+$podil = $a-$b;
+
+$rozdil = $a/$b;
+
+ ?>
+<p><?php
+echo "$a + $b = $soucet"
+?>
+
+<p><?php
+echo "$a * $b = $soucin"
+?>
+
+<p><?php
+echo "$a - $b = $podil"
+ ?>
+
+ <p><?php
+echo "$a / $b = $rozdil"
+ ?>
+
+</body>
+
+</html>
